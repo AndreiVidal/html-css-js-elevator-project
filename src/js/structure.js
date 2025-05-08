a2f71b1bee45c1be73e809e3ebd26bb7b1d1dc94
Andrei Vidal <andrei.godoi92@gmail.com>
2025-05-08 20:54:42 -0300

    :sparkles: feat: add dynamic elevator creation in structure.js

--- src/js/structure.js
+++ src/js/structure.js
@@ -5,20 +5,35 @@
 
     const lobby = document.createElement("div");
     lobby.classList.add("lobby");
+    lobby.setAttribute("floor", "t");
     lobby.appendChild(window);
 
     return lobby;
   }
+  function getElevatorSize() {
+    const elevatorSize = document.querySelector('[floor="t"]');
+    return elevatorSize.offsetHeight + "px";
+  }
 
-  function createdFloor() {
+  function createElevator() {
+    const buildingElevator = document.querySelector(".building-elevator");
+    const elevator = document.createElement("div");
+    elevator.classList.add("elevator");
+    elevator.style.height = getElevatorSize();
+    buildingElevator.appendChild(elevator);
+  }
+
+  function createdFloor(numberFloor) {
     const door = document.createElement("div");
     door.classList.add("door");
 
     const floor = document.createElement("div");
     floor.classList.add("floor");
+    floor.setAttribute("floor", numberFloor);
     floor.appendChild(door);
     return floor;
   }
+
   function createButtons(qtd, container) {
     container.innerHTML = "";
     for (let i = qtd; i >= 1; i--) {
@@ -34,7 +49,6 @@
   function buildingFloor() {
     const elFloor = document.querySelectorAll("[floor]");
     const controlButtons = document.querySelector(".control-buttons");
-    const displayPanel = document.querySelector(".display-panel");
     const qtd = +elFloor[0].getAttribute("floor");
 
     if (controlButtons) {
@@ -44,11 +58,12 @@
     elFloor.forEach((el) => {
       const qtd = +el.getAttribute("floor");
       for (let i = qtd; i > 0; i--) {
-        el.appendChild(createdFloor());
+        el.appendChild(createdFloor(i));
       }
       el.appendChild(createdLobby());
     });
   }
 
   buildingFloor();
+  createElevator();
 })();
